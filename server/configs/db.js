33884e2a41555e887e71977110cf2657b889a6bb
Alokkagarwal18 <Alokagarwal1812@gmail.com>
2025-09-14 02:51:41 +0530

    Fix Clerk webhook type and safe email access

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
+const connectDB = async () => {
   try {
-    mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-    await mongoose.connect(process.env.MONGODB_URI)
+    mongoose.connection.on("connected", () => console.log("Database Connected"));
+    mongoose.connection.on("error", (err) => console.log("DB Connection Error:", err));
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 10s timeout
+    });
   } catch (error) {
     console.log(error.message);
   }
-}
+};
 
 export default connectDB;
